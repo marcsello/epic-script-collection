88a7981d255be533befe7ed5cdbd77c34bd10227
Marcsello <marcsello@users.noreply.github.com>
2017-08-07 11:47:51 +0200

    Updated for systemd; added logrotate and suppress clean
    Changed pidof+service commands to systemctl, now it won't confuse process names with service names
    
    Added logrotate function, now if it exceeds log length it will remove old entries
    
    Added feature to automatically remove services from the suppress list, if they appear to be running again. no need for manual removing of the suppress file anymore.
    
    Also small fixes, optimizations and improvements

--- service_watchdog.sh
+++ service_watchdog.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# set service names here
-SERVICES=( "apache2" "vsftpd" "sshd" "smbd" "nmbd" "cykabylat" );
+# configuration -------------------------------------------------------------------------------------------
 
-# mail about failure will sent to this user;leave empty to disable this feature
-SYSADMIN_NAME="marcsello"
+# set service names here
+SERVICES=( "apache2" "vsftpd" "sshd" "smbd" "nmbd" );
+
+# mail about failure will sent to this user; leave empty to disable this feature
+SYSADMIN_NAME=""
 
 # used to store service restart failures
 SUPPRESS_ERRORS="/var/service_watchdog.supress"
@@ -12,36 +14,67 @@ SUPPRESS_ERRORS="/var/service_watchdog.supress"
 # log... must rotate somehow
 LOGFILE_PATH="/var/log/service_watchdog.log"
 
-# no need to bother this part
+# maximum lines of the logfile; old entries will be removed
+LOGFILE_MAX_LINES="2000"
 
-# echo "$('date') Service Watchdog run" >> "$LOGFILE_PATH"
+# configuration ends here  -------------------------------------------------------------------------------
 
-if [ ! -f "$SUPPRESS_ERRORS" ]; then
-	touch "$SUPPRESS_ERRORS"
-fi
+# prepare
 
-MESSAGE_SUFFIX=" fails to restart!"
+[ ! -f "$SUPPRESS_ERRORS" ] && touch "$SUPPRESS_ERRORS"
+[ ! -f "$LOGFILE_PATH" ] && touch "$LOGFILE_PATH"
+
+# actual job
+
 for service in "${SERVICES[@]}"; do
-	pidof "$service" > /dev/null
-	if [ "$?" != "0" ]; then #if not running (pidof returns not zero)
 
-		# check if this process failed to restart in the past
-		cat "$SUPPRESS_ERRORS" | grep "$service" > /dev/null 2>&1
+        systemctl status "$service" > /dev/null 2>&1
 
-		if [ "$?" != "0" ]; then # this service is not failed to restart yet
-			service "$service" restart > /dev/null 2>&1 # so we try to restart it
-			if [ "$?" != "0" ]; then  # failed to restart, report it! and add to suppress list
-				REPORT_MSG="$service $MESSAGE_SUFFIX"
-				echo "$('date') $REPORT_MSG" >> "$LOGFILE_PATH"
-				if [ "$SYSADMIN_NAME" != "" ]; then
-					printf "$REPORT_MSG\nPlease do \"rm $SUPPRESS_ERRORS\" after fixing!" | mail mail -s "Service failure" "$SYSADMIN_NAME"
-				fi
-				echo "$service" >> "$SUPPRESS_ERRORS"
-			else # restart was a success
-				echo "$('date') $service found dead restarting it was successful!" >> "$LOGFILE_PATH"
-			fi
+        if [ "$?" != "0" ]; then # if not running (status returns non-zero)
 
-		fi # if it does failed to restart in the past, we leave it alone
+                # check if this process failed to restart in the past
+                cat "$SUPPRESS_ERRORS" | grep "$service" > /dev/null 2>&1
 
-	fi
-done 
+                if [ "$?" != "0" ]; then # this service is not failed to restart yet
+
+                 systemctl start "$service" > /dev/null 2>&1 # so we try to restart it
+
+                 if [ "$?" != "0" ]; then  # failed to restart, report it! and add to suppress list
+                        REPORT_MSG="$service fails to restart!"
+                        echo "$('date') $REPORT_MSG" >> "$LOGFILE_PATH"
+                        if [ "$SYSADMIN_NAME" != "" ]; then
+                            echo "$REPORT_MSG" | mail mail -s "Service failure" "$SYSADMIN_NAME"
+                        fi
+                        echo "$service" >> "$SUPPRESS_ERRORS" # record it
+                 else # restart was a success
+                        echo "$('date') $service found dead restarting it was successful!" >> "$LOGFILE_PATH"
+                 fi
+
+                fi # if it does failed to restart in the past, we leave it alone
+
+        else # it's running now
+
+                cat "$SUPPRESS_ERRORS" | grep "$service" > /dev/null 2>&1
+
+                if [ "$?" == "0" ]; then # it was suppressed before... but now it's running, yay
+
+                 sed -i "/$service/d" "$SUPPRESS_ERRORS"
+                 echo "$('date') $service is doing fine, removed from suppress list" >> "$LOGFILE_PATH"
+
+                fi
+
+
+        fi
+done
+
+
+# rotate logfile
+
+logfile_len=$(cat "$LOGFILE_PATH" | wc -l)
+
+if [ "$logfile_len" -gt "$LOGFILE_MAX_LINES" ]; then
+
+        logfile_tmp=$(tail -n "$LOGFILE_MAX_LINES" "$LOGFILE_PATH") # must store it temporarly, directly piping it back causes an empty file
+        echo "${logfile_tmp}" > "$LOGFILE_PATH"
+
+fi
